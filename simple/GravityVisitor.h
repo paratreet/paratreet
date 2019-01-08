b07a617ff6660071e0043ad04451017a183ddaaa
Joseph Hutter <josephahutter@gmail.com>
2019-01-07 22:53:50 -0600

    Small bug fix to GravityVisitor

--- simple/GravityVisitor.h
+++ simple/GravityVisitor.h
@@ -17,15 +17,16 @@ private:
     return dsq;
   }
   void addGravity(const Node<CentroidData>* source, Node<CentroidData>* target, std::vector<Vector3D<Real> >& sum_forces, bool isLeaf) {
-    const Real gconst = 0.000000000066742; // very small
-    sum_forces.resize(target->n_particles);
+    const Real gconst = 0.1 ;// 0.000000000066742; // very small
+    sum_forces.resize(target->n_particles, Vector3D<Real>(0,0,0));
     for (int i = 0; i < target->n_particles; i++) {
       if (isLeaf) {
         for (int j = 0; j < source->n_particles; j++) {
-          if (target->particles[i].key == target->particles[j].key) continue;
+          if (target->particles[i].key == source->particles[j].key) continue;
           Real rsq = distsq(source->particles[j].position, target->particles[i].position);
-          sum_forces[i] += (source->particles[j].position - target->particles[i].position)
+          Vector3D<Real> curr_force = (source->particles[j].position - target->particles[i].position)
             * gconst * source->particles[j].mass * target->particles[i].mass / (rsq * std::sqrt(rsq));
+          sum_forces[i] += curr_force;
         }
       }
       else {
@@ -38,13 +39,14 @@ private:
 public:
   GravityVisitor() {}
   void leaf(const Node<CentroidData>* source, Node<CentroidData>* target) { // we're calculating force from node X on node Y
-    centroid_driver.countLeaf();
+    //centroid_driver.countLeaf();
     addGravity(source, target, target->sum_forces, true);
   }
   bool node(const Node<CentroidData>* source, Node<CentroidData>* target) {
-    const Real theta = .5, total_volume = 41050;
+    const Real theta = 0.5, total_volume = 41050;
     Real s = std::pow(total_volume, 1/3.) * std::pow(2, -1 * Utility::getDepthFromKey(source->key));
-    if (theta == 0 || distsq(source->data.getCentroid(), target->data.getCentroid()) < s * s / (theta * theta)) {
+    Real soverd = s / std::sqrt(distsq(source->data.getCentroid(), target->data.getCentroid()));
+    if (soverd > theta) {
       return true;
     }
     addGravity(source, target, target->sum_forces, false);
