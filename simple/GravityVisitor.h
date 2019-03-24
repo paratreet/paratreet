95aeaf59467937fc0a1550a34a9c8af06871214d
Joseph Hutter <tg850493@c455-082.stampede2.tacc.utexas.edu>
2019-03-24 14:12:09 -0500

    Gravity Visitor slight rework

--- simple/GravityVisitor.h
+++ simple/GravityVisitor.h
@@ -11,40 +11,39 @@ extern CProxy_CacheManager<CentroidData> centroid_cache;
 
 class GravityVisitor {
 private:
-  void addGravity(const Node<CentroidData>* __restrict__ source, Node<CentroidData>* __restrict__ target, std::vector<Vector3D<Real> >& sum_forces, bool isLeaf) {
-    const Real gconst = 0.1 ;// 0.000000000066742; // very small
-    sum_forces.resize(target->n_particles, Vector3D<Real>(0,0,0));
+  const Real gconst = 0.000000000066742;
+  const Real theta = 0.5;
+  void addGravityLeaf(const Node<CentroidData>* __restrict__ source, Node<CentroidData>* __restrict__ target, std::vector<Vector3D<Real> >& sum_forces) {
     for (int i = 0; i < target->n_particles; i++) {
-      //centroid_cache.ckLocalBranch()->countInt(isLeaf);
-      if (isLeaf) {
-        for (int j = 0; j < source->n_particles; j++) {
-          if (target->particles[i].key == source->particles[j].key) continue;
-	  Vector3D<Real> diff = source->particles[j].position - target->particles[i].position;
-          Real rsq = diff.lengthSquared();
-	  sum_forces[i] += diff * (gconst * source->particles[j].mass * target->particles[i].mass / (rsq * sqrt(rsq)));
-        }
-      }
-      else {
-        Vector3D<Real> diff = source->data.getCentroid() - target->particles[i].position;
+      //centroid_cache.ckLocalBranch()->countInt(1);
+      for (int j = 0; j < source->n_particles; j++) {
+        if (target->particles[i].key == source->particles[j].key) continue;
+	Vector3D<Real> diff = source->particles[j].position - target->particles[i].position;
         Real rsq = diff.lengthSquared();
-        sum_forces[i] += diff * (gconst * source->data.sum_mass * target->particles[i].mass / (rsq * sqrt(rsq)));
+	sum_forces[i] += diff * (gconst * source->particles[j].mass * target->particles[i].mass / (rsq * sqrt(rsq)));
       }
     }
   }
-public:
+  void addGravityNode(const Node<CentroidData>* __restrict__ source, Node<CentroidData>* __restrict__ target, std::vector<Vector3D<Real> >& sum_forces) {
+    for (int i = 0; i < target->n_particles; i++) {
+      Vector3D<Real> diff = source->data.getCentroid() - target->particles[i].position;
+      Real rsq = diff.lengthSquared();
+      sum_forces[i] += diff * (gconst * source->data.sum_mass * target->particles[i].mass / (rsq * sqrt(rsq)));
+    }
+  }
+  public:
   GravityVisitor() {}
-  void leaf(const Node<CentroidData>* source, Node<CentroidData>* target) { // we're calculating force from node X on node Y
+  void leaf(const Node<CentroidData>* source, Node<CentroidData>* target) { // we're calculating force from node source on node target
     //centroid_driver.countLeaf();
-    addGravity(source, target, target->sum_forces, true);
+    addGravityLeaf(source, target, target->sum_forces);
   }
   bool node(const Node<CentroidData>* source, Node<CentroidData>* target) {
-    const Real theta = 0.5;
     Vector3D<Real> dr = source->data.getCentroid() - target->data.getCentroid();
     Real dsq = dr.lengthSquared();
     if (theta * dsq < source->data.rsq) { 
       return true;
     }
-    addGravity(source, target, target->sum_forces, false);
+    addGravityNode(source, target, target->sum_forces);
     return false;
   }
   bool cell(std::pair<Key, const CentroidData> source, std::pair<Key, const CentroidData> target) {
@@ -52,7 +51,6 @@ public:
     // check all eight corners of bounding box
     // return true if one corner is within total_volume of node
     // else return false
-    const Real theta = 0.5;
     const OrientedBox<Real> box = target.second.box;
     for (int i = 0; i < 2; i++) {
       for (int j = 0; j < 2; j++) {
