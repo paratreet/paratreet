af2bd97ff4a690779bab1865aefeae853351af6b
mojoe12 <josephahutter@gmail.com>
2020-06-20 13:32:19 -0500

    explain batched resume

--- src/Resumer.h
+++ src/Resumer.h
@@ -41,7 +41,12 @@ public:
     auto it = waiting.find(key);
     if (it == waiting.end()) return;
     for (auto tp_index : it->second) {
-      resume_nodes_per_tp[tp_index].push(node);
+      auto && resume_nodes = resume_nodes_per_tp[tp_index];
+      resume_nodes.push(node);
+      // batched resume logic would go here --
+      // if (resume_nodes > RESUME_BATCH_SIZE)
+      // but I am not sure how to tune it so that
+      // we actually complete on the last few reusmes
       tp_proxy[tp_index].goDown(key);
     }
     waiting.erase(it);
