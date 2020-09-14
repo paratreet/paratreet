51c8c72f18bca4368d817be8525129d34fe0a655
Justin Szaday <wingpad@gmail.com>
2020-09-14 11:14:30 -0500

    use tree's branch factor to select nodes

--- src/TreeSpec.h
+++ src/TreeSpec.h
@@ -9,7 +9,8 @@ class TreeSpec : public CBase_TreeSpec {
 public:
     TreeSpec(const paratreet::Configuration& config_)
     : config(config_),
-      decomp(nullptr) { }
+      decomp(nullptr),
+      tree(nullptr) { getTree(); }
 
     void receiveDecomposition(CkMarshallMsg*);
     Decomposition* getDecomposition();
@@ -22,8 +23,16 @@ public:
 
     template <typename Data>
     Node<Data>* makeNode(Key key, int depth, int n_particles, Particle* particles, int owner_tp_start, int owner_tp_end, bool is_leaf, Node<Data>* parent, int tp_index) const {
-      return new FullNode<Data, 8> (key, depth, n_particles, particles, owner_tp_start, owner_tp_end, is_leaf, parent, tp_index);
+      switch (tree->getBranchFactor()) {
+      case 2:
+        return new FullNode<Data, 2> (key, depth, n_particles, particles, owner_tp_start, owner_tp_end, is_leaf, parent, tp_index);
+      case 8:
+        return new FullNode<Data, 8> (key, depth, n_particles, particles, owner_tp_start, owner_tp_end, is_leaf, parent, tp_index);
+      default:
+        return nullptr;
+      }
     }
+
     template <typename Data>
     Node<Data>* makeCachedNode(Key key, typename Node<Data>::Type type, SpatialNode<Data> spatial_node, Node<Data>* parent, const Particle* particlesToCopy) {
       Particle* particles = nullptr;
@@ -31,7 +40,14 @@ public:
         particles = new Particle [spatial_node.n_particles];
         std::copy(particlesToCopy, particlesToCopy + spatial_node.n_particles, particles);
       }
-      return new FullNode<Data, 8> (key, type, spatial_node.is_leaf, spatial_node, particles, parent);
+      switch (tree->getBranchFactor()) {
+      case 2:
+        return new FullNode<Data, 2> (key, type, spatial_node.is_leaf, spatial_node, particles, parent);
+      case 8:
+        return new FullNode<Data, 8> (key, type, spatial_node.is_leaf, spatial_node, particles, parent);
+      default:
+        return nullptr;
+      }
     }
 
 protected:
