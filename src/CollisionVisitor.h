aa5f1c16180de9caf8ece2f5c50db37be49397dc
Spencer Wallace <scw7@uw.edu>
2020-09-28 17:06:27 -0700

    Clean up fixed ball search, allow self to be neighbor

--- src/CollisionVisitor.h
+++ src/CollisionVisitor.h
@@ -35,9 +35,6 @@ public:
       for (int j = 0; j < source.n_particles; j++) {
         Real dsq = (target.particles()[i].position - source.particles()[j].position).lengthSquared();
         Real rsq = target.particles()[i].ball*target.particles()[i].ball;
-        // A particle cant be its own neighbor
-        if (target.particles()[i].order == source.particles()[j].order)
-          continue;
         if (dsq < rsq) {
           target.data.fixed_ball[i].push_back(source.particles()[j]);
           // For now, just print out the orders of the neighbors to check correctness
@@ -52,36 +49,6 @@ public:
   }
 
 private:
-/*
- * Return true if a sphere centered at pos with radius^2 of rsq
- * intersects the bounding box of node.
- * This is being used instead of the Space intersect method so we can use
- * radius^2 for efficiency.
- * Copied from smooth.cpp in ChaNGa
- */
-/*static inline bool
-intersect(const OrientedBox<Real>& box, Vector3D<Real> pos, Real rsq) {
-    Real dsq = 0.0;
-    Real delta;
-    
-    if((delta = box.lesser_corner.x - pos.x) > 0)
-	dsq += delta * delta;
-    else if((delta = pos.x - box.greater_corner.x) > 0)
-	dsq += delta * delta;
-    if(rsq < dsq)
-	return false;
-    if((delta = box.lesser_corner.y - pos.y) > 0)
-	dsq += delta * delta;
-    else if((delta = pos.y - box.greater_corner.y) > 0)
-	dsq += delta * delta;
-    if(rsq < dsq)
-	return false;
-    if((delta = box.lesser_corner.z - pos.z) > 0)
-	dsq += delta * delta;
-    else if((delta = pos.z - box.greater_corner.z) > 0)
-	dsq += delta * delta;
-    return (dsq <= rsq);
-    }*/
 };
 
 #endif // PARATREET_COLLISIONVISITOR_H_
