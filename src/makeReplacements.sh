a0fc000908d24a4c990107ac4af4a95deb610d93
Justin Szaday <wingpad@gmail.com>
2020-11-04 19:15:16 -0600

    A temporary, hacky fix to get things up and going

---
+++ src/makeReplacements.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DECL_H="paratreet.decl.h"
+DEF_H="paratreet.def.h"
+
+lineNum="$(grep -n "void recursive_pup_impl<ArrayMeshStreamer<Key, TreePiece <" "$DECL_H" | head -n 1 | cut -d: -f1)"
+sed -i "$lineNum""s/Data/CentroidData/g" "$DECL_H"
+
+lineNum="$(grep -n "void recursive_pup_impl<ArrayMeshStreamer<Key, TreePiece <" "$DEF_H" | head -n 1 | cut -d: -f1)"
+sed -i "$lineNum""s/Data/CentroidData/g" "$DEF_H"
+
+lineNum="$(grep -n "obj->ArrayMeshStreamer<Key, TreePiece <" "$DEF_H" | head -n 1 | cut -d: -f1)"
+sed -i "$lineNum""s/Data/CentroidData/g" "$DEF_H"
+
+lineNum="$(grep -n "goDown_marshall.*TramAggregator->insertData" "$DEF_H" | head -n 1 | cut -d: -f1)"
+sed -i "$lineNum""s/<false>/ /g" "$DEF_H"
