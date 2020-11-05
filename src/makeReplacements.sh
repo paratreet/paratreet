d146c380d534d5333d5a1cb696c34e9ca4f0781d
Justin Szaday <wingpad@gmail.com>
2020-11-05 09:42:01 -0600

    switched to using awk with grep as a fallback

--- src/makeReplacements.sh
+++ src/makeReplacements.sh
@@ -1,16 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash
 
 DECL_H="paratreet.decl.h"
 DEF_H="paratreet.def.h"
 
-lineNum="$(grep -n "void recursive_pup_impl<ArrayMeshStreamer<Key, TreePiece <" "$DECL_H" | head -n 1 | cut -d: -f1)"
-sed -i "$lineNum""s/Data/CentroidData/g" "$DECL_H"
+function replace {
+    lineNum="$(awk "/$1/{ print NR; exit }" $2)"
+    if test -z "$lineNum"; then
+        lineNum="$(grep -n "$1" "$2" | head -n 1 | cut -d: -f1)"
+    fi
+    if test -z "$lineNum"; then
+        >&2 echo "ERROR: Could not find match for pattern: $1"
+        exit 1 # terminate and indicate error
+    else
+        sed -i "$lineNum""s/$3/$4/g" "$2"
+    fi
+}
 
-lineNum="$(grep -n "void recursive_pup_impl<ArrayMeshStreamer<Key, TreePiece <" "$DEF_H" | head -n 1 | cut -d: -f1)"
-sed -i "$lineNum""s/Data/CentroidData/g" "$DEF_H"
-
-lineNum="$(grep -n "obj->ArrayMeshStreamer<Key, TreePiece <" "$DEF_H" | head -n 1 | cut -d: -f1)"
-sed -i "$lineNum""s/Data/CentroidData/g" "$DEF_H"
-
-lineNum="$(grep -n "goDown_marshall.*TramAggregator->insertData" "$DEF_H" | head -n 1 | cut -d: -f1)"
-sed -i "$lineNum""s/<false>/ /g" "$DEF_H"
+replace "void recursive_pup_impl<ArrayMeshStreamer<Key, TreePiece <" "$DECL_H" "Data" "CentroidData"
+replace "void recursive_pup_impl<ArrayMeshStreamer<Key, TreePiece <" "$DEF_H" "Data" "CentroidData"
+replace "obj->ArrayMeshStreamer<Key, TreePiece <" "$DEF_H" "Data" "CentroidData"
+replace "goDown_marshall.*TramAggregator->insertData" "$DEF_H" "<false>" " "
