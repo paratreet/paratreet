4acba8e95059dc488479da646e7d9e6b09bbfadb
mojoe12 <josephahutter@gmail.com>
2020-12-15 21:52:43 -0600

    decomposition speedups in Driver

--- src/Driver.h
+++ src/Driver.h
@@ -72,10 +72,6 @@ public:
     cb.send();
   }
 
-  void broadcastDecomposition(const CkCallback& cb, CProxy_TreeSpec& treespec) {
-    treespec.receiveDecomposition(cb, CkPointer<Decomposition>(treespec.ckLocalBranch()->getDecomposition()));
-  }
-
   // Performs decomposition by distributing particles among Subtrees,
   // by either loading particle information from input file or re-computing
   // the universal bounding box
@@ -112,17 +108,19 @@ public:
     // Set up splitters for decomposition
     start_time = CkWallTimer();
     n_subtrees = treespec_subtrees.ckLocalBranch()->doFindSplitters(universe, readers);
-    broadcastDecomposition(CkCallbackResumeThread(), treespec_subtrees);
-    CkPrintf("Setting up splitters for subtree decomposition: %.3lf ms\n",
-        (CkWallTimer() - start_time) * 1000);
-
-    start_time = CkWallTimer();
-    n_partitions = treespec.ckLocalBranch()->doFindSplitters(universe, readers);
-    treespec.ckLocalBranch()->getDecomposition()->alignSplitters(
-      treespec_subtrees.ckLocalBranch()->getDecomposition()
-      );
-    broadcastDecomposition(CkCallbackResumeThread(), treespec);
-    CkPrintf("Setting up splitters for partition decomposition: %.3lf ms\n",
+    treespec_subtrees.receiveDecomposition(CkCallbackResumeThread(),
+      CkPointer<Decomposition>(treespec_subtrees.ckLocalBranch()->getDecomposition()));
+    if (config.decomp_type == OCT_DECOMP) {
+      n_partitions = n_subtrees;
+      treespec.receiveDecomposition(CkCallbackResumeThread(),
+        CkPointer<Decomposition>(treespec_subtrees.ckLocalBranch()->getDecomposition()));
+    }
+    else {
+      n_partitions = treespec.ckLocalBranch()->doFindSplitters(universe, readers);
+      treespec.receiveDecomposition(CkCallbackResumeThread(),
+        CkPointer<Decomposition>(treespec.ckLocalBranch()->getDecomposition()));
+    }
+    CkPrintf("Setting up splitters for particle decompositions: %.3lf ms\n",
         (CkWallTimer() - start_time) * 1000);
 
     // Create Subtrees
