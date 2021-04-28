a0b0f37173b3da467ee2dbedd269ca3324a06813
Joseph Hutter <tg850493@login1.stampede2.tacc.utexas.edu>
2021-04-28 08:39:24 -0500

    add some moments function impls

--- src/moments.h
+++ src/moments.h
@@ -1,11 +1,7 @@
 #ifndef MOMENTS_INCLUDED
 #define MOMENTS_INCLUDED
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
-#include "cosmoType.h"
+#include "common.h"
 
 /*************************************************
  *
@@ -27,38 +23,38 @@ typedef double momFloat;
 /**
  ** @brief moment tensor components for reduced multipoles.
  */
-typedef struct momReduced {
-	momFloat m;
-	momFloat xx,yy,xy,xz,yz;
-	momFloat xxx,xyy,xxy,yyy,xxz,yyz,xyz;
-	momFloat xxxx,xyyy,xxxy,yyyy,xxxz,yyyz,xxyy,xxyz,xyyz;
-	} MOMR;
+struct MOMR {
+  momFloat m;
+  momFloat xx,yy,xy,xz,yz;
+  momFloat xxx,xyy,xxy,yyy,xxz,yyz,xyz;
+  momFloat xxxx,xyyy,xxxy,yyyy,xxxz,yyyz,xxyy,xxyz,xyyz;
+};
 
 /**
  ** @brief moment tensor components for complete multipoles.
  */
-typedef struct momComplete {
-	momFloat m;
-	momFloat xx,yy,xy,xz,yz;
-	momFloat xxx,xyy,xxy,yyy,xxz,yyz,xyz;
-	momFloat xxxx,xyyy,xxxy,yyyy,xxxz,yyyz,xxyy,xxyz,xyyz;
-	momFloat zz;
-	momFloat xzz,yzz,zzz;
-	momFloat xxzz,xyzz,xzzz,yyzz,yzzz,zzzz;
-	} MOMC;
+struct MOMC {
+  momFloat m;
+  momFloat xx,yy,xy,xz,yz;
+  momFloat xxx,xyy,xxy,yyy,xxz,yyz,xyz;
+  momFloat xxxx,xyyy,xxxy,yyyy,xxxz,yyyz,xxyy,xxyz,xyyz;
+  momFloat zz;
+  momFloat xzz,yzz,zzz;
+  momFloat xxzz,xyzz,xzzz,yyzz,yzzz,zzzz;
+};
 
 /**
  ** @brief moment tensor components for reduced local expansion.
  ** note that we have the 5th-order terms here now!
  */
-typedef struct locReduced {
-    momFloat m;
-    momFloat x,y,z;
-    momFloat xx,xy,yy,xz,yz;
-    momFloat xxx,xxy,xyy,yyy,xxz,xyz,yyz;
-    momFloat xxxx,xxxy,xxyy,xyyy,yyyy,xxxz,xxyz,xyyz,yyyz;
-    momFloat xxxxx,xxxxy,xxxyy,xxyyy,xyyyy,yyyyy,xxxxz,xxxyz,xxyyz,xyyyz,yyyyz;
-    } LOCR;
+struct LOCR {
+  momFloat m;
+  momFloat x,y,z;
+  momFloat xx,xy,yy,xz,yz;
+  momFloat xxx,xxy,xyy,yyy,xxz,xyz,yyz;
+  momFloat xxxx,xxxy,xxyy,xyyy,yyyy,xxxz,xxyz,xyyz,yyyz;
+  momFloat xxxxx,xxxxy,xxxyy,xxyyy,xyyyy,yyyyy,xxxxz,xxxyz,xxyyz,xyyyz,yyyyz;
+};
 
 /*
 ** The next set of data structures are intended specifically for use with float
@@ -66,53 +62,52 @@ typedef struct locReduced {
 ** cell or volume. The convention is to use the scaling factor u for the multipole
 ** moments and scaling factor v for the local expansion.
 */
-typedef struct fmomReduced {
-    cosmoType m;
-    cosmoType xx, yy, xy, xz, yz;
-    cosmoType xxx, xyy, xxy, yyy, xxz, yyz, xyz;
-    cosmoType xxxx, xyyy, xxxy, yyyy, xxxz, yyyz, xxyy, xxyz, xyyz;
-    } FMOMR;
+struct FMOMR {
+  Real m;
+  Real xx, yy, xy, xz, yz;
+  Real xxx, xyy, xxy, yyy, xxz, yyz, xyz;
+  Real xxxx, xyyy, xxxy, yyyy, xxxz, yyyz, xxyy, xxyz, xyyz;
+};
 
-typedef struct flocReduced {
-    cosmoType m;
-    cosmoType x, y, z;
-    cosmoType xx, yy, xy, xz, yz;
-    cosmoType xxx, xyy, xxy, yyy, xxz, yyz, xyz;
-    cosmoType xxxx, xyyy, xxxy, yyyy, xxxz, yyyz, xxyy, xxyz, xyyz;
-    cosmoType xxxxx, xyyyy, xxxxy, yyyyy, xxxxz, yyyyz, xxxyy, xxyyy, xxxyz,
-        xyyyz, xxyyz;
-    } FLOCR;
+struct FLOCR {
+  Real m;
+  Real x, y, z;
+  Real xx, yy, xy, xz, yz;
+  Real xxx, xyy, xxy, yyy, xxz, yyz, xyz;
+  Real xxxx, xyyy, xxxy, yyyy, xxxz, yyyz, xxyy, xxyz, xyyz;
+  Real xxxxx, xyyyy, xxxxy, yyyyy, xxxxz, yyyyz, xxxyy, xxyyy, xxxyz, xyyyz, xxyyz;
+};
 
 void momClearMomr(MOMR *mr);
 void momClearFmomr(FMOMR *l);
 void momAddMomc(MOMC *,MOMC *);
 void momAddMomr(MOMR *,MOMR *);
 void momAddFmomr(FMOMR *mr,FMOMR *ma);
-void momScaledAddFmomr(FMOMR *mr, cosmoType ur, FMOMR *ma, cosmoType ua);
-void momRescaleFmomr(FMOMR *mr, cosmoType unew, cosmoType uold);
+void momScaledAddFmomr(FMOMR *mr, Real ur, FMOMR *ma, Real ua);
+void momRescaleFmomr(FMOMR *mr, Real unew, Real uold);
 void momMulAddMomc(MOMC *,momFloat,MOMC *);
 void momMulAddMomr(MOMR *,momFloat,MOMR *);
-void momMulAddFmomr(FMOMR *mr, cosmoType ur, cosmoType m, FMOMR *ma,
-                    cosmoType ua);
+void momMulAddFmomr(FMOMR *mr, Real ur, Real m, FMOMR *ma,
+                    Real ua);
 void momSubMomc(MOMC *,MOMC *);
 void momSubMomr(MOMR *,MOMR *);
-void momScaledSubFmomr(FMOMR *mr, cosmoType ur, FMOMR *ma, cosmoType ua);
+void momScaledSubFmomr(FMOMR *mr, Real ur, FMOMR *ma, Real ua);
 void momMakeMomc(MOMC *,momFloat,momFloat,momFloat,momFloat);
-cosmoType momMakeFmomr(FMOMR *mr, cosmoType m, cosmoType u, cosmoType x,
-                       cosmoType y, cosmoType z);
+Real momMakeFmomr(FMOMR *mr, Real m, Real u, Real x,
+                       Real y, Real z);
 momFloat momMakeMomr(MOMR *,momFloat,momFloat,momFloat,momFloat);
 void momOldMakeMomr(MOMR *,momFloat,momFloat,momFloat,momFloat);
 void momShiftMomc(MOMC *,momFloat,momFloat,momFloat);
 void momShiftMomr(MOMR *,momFloat,momFloat,momFloat);
-void momShiftFmomr(FMOMR *m, cosmoType u, cosmoType x, cosmoType y,
-                   cosmoType z);
+void momShiftFmomr(FMOMR *m, Real u, Real x, Real y,
+                   Real z);
 double momShiftLocr(LOCR *,momFloat,momFloat,momFloat);
 void momReduceMomc(MOMC *,MOMR *);
 void momEvalMomr(MOMR *,momFloat,momFloat,momFloat,momFloat,
 				 momFloat *,momFloat *,momFloat *,momFloat *);
-void momEvalFmomrcm(FMOMR *m, cosmoType u, cosmoType dir, cosmoType x,
-                    cosmoType y, cosmoType z, cosmoType *fPot, cosmoType *ax,
-                    cosmoType *ay, cosmoType *az, cosmoType *magai);
+void momEvalFmomrcm(FMOMR *m, Real u, Real dir, Real x,
+                    Real y, Real z, Real *fPot, Real *ax,
+                    Real *ay, Real *az, Real *magai);
 void momMomr2Momc(MOMR *,MOMC *);
 void momFmomr2Momc(FMOMR *ma,MOMC *mc);
 void momPrintMomc(MOMC *);
@@ -120,13 +115,12 @@ void momPrintMomr(MOMR *);
 
 void momClearLocr(LOCR *);
 double momLocrAddMomr5(LOCR *,MOMR *,momFloat,momFloat,momFloat,momFloat,double *,double *,double *);
-double momFlocrAddFmomr5cm(FLOCR *l, cosmoType v, FMOMR *m, cosmoType u,
-                           cosmoType dir, cosmoType x, cosmoType y, cosmoType z,
-                           cosmoType *tax, cosmoType *tay, cosmoType *taz);
+double momFlocrAddFmomr5cm(FLOCR *l, Real v, FMOMR *m, Real u,
+                           Real dir, Real x, Real y, Real z,
+                           Real *tax, Real *tay, Real *taz);
 void momEvalLocr(LOCR *,momFloat,momFloat,momFloat,
 		 momFloat *,momFloat *,momFloat *,momFloat *);
 double momLocrAddMomr(LOCR *,MOMR *,momFloat,momFloat,momFloat,momFloat);
-#if defined(__cplusplus)
-}
-#endif
-#endif
+
+
+#endif // MOMENTS_INCLUDED
