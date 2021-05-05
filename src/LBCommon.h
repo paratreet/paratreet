fd5017b0baa5e6e5b2e81f7931e82b08be93c6f5
Simeng Liu <simengl2@illinois.edu>
2021-05-05 12:00:43 -0500

    add LBCommon

--- src/LBCommon.h
+++ src/LBCommon.h
@@ -6,13 +6,13 @@
 namespace LBCommon{
 
   enum LBType {
-    pt = 1111, // Partition
+    pt = 1111, // Partition{{{
     st = 2222, // Subtree
     cp = 3333  // TreeCanopy
-  };
+  };/*}}}*/
 
   struct LBUserData{
-    int lb_type;
+    int lb_type;/*{{{*/
     int chare_idx;
     int particle_size;
     int particle_sum;
@@ -26,10 +26,10 @@ namespace LBCommon{
       p | particle_sum;
       p | centroid;
     };
-  };
+  };/*}}}*/
 
   struct LBCompareStats{
-    int index; // index from objData
+    int index; // index from objData{{{
     int chare_idx; // index from Subtree or Partition chare arrays
     int partical_size;
     double load;
@@ -37,40 +37,49 @@ namespace LBCommon{
     int from_proc;
     Vector3D<Real> centroid;
     inline void pup(PUP::er &p);
-  };
+  };/*}}}*/
 
   struct LBCentroidRecord{
-    Vector3D<Real> centroid;
+    Vector3D<Real> centroid;/*{{{*/
     int from_pe;
     inline void pup(PUP::er &p);
-  };
+  };/*}}}*/
+
+  struct LBCentroidAndIndexRecord{
+    Vector3D<Real> centroid;/*{{{*/
+    int idx;
+    int particle_size;
+    int from_pe;
+    float load;
+    inline void pup(PUP::er &p);
+  };/*}}}*/
 
   struct LBCentroidCompare{
-    Real distance;
+    Real distance;/*{{{*/
     int from_pe;
     inline void pup(PUP::er &p);
-  };
+  };/*}}}*/
 };
 
 namespace{
   bool CompareLBStats(const LBCommon::LBCompareStats & a, const LBCommon::LBCompareStats & b)
-  {
+  {/*{{{*/
     // compare chare array index
     return a.chare_idx < b.chare_idx;
-  };
+  };/*}}}*/
 
   bool ComparePEAndChareidx(const LBCommon::LBCompareStats & a, const LBCommon::LBCompareStats &b)
-  {
+  {/*{{{*/
     // first compare PE
     int a_pe = a.from_proc, b_pe = b.from_proc;
     if (a_pe != b_pe) return a_pe < b_pe;
 
     // then compare chare array index
     return a.chare_idx < b.chare_idx;
-  };
+  };/*}}}*/
 
   bool PrefixLBCompareLDObjStats(const LDObjStats & a, const LDObjStats & b)
-  {
+  {/*{{{*/
     // first compare PE
     int a_pe = a.from_proc, b_pe = b.from_proc;
     if (a_pe != b_pe) return a_pe < b_pe;
@@ -78,12 +87,24 @@ namespace{
     // then compare chare array index
     // to_proc is assigned with chare_idx from user data
     return a.to_proc < b.to_proc;
-  };
+  };/*}}}*/
 
   bool CentroidRecordCompare(const LBCommon::LBCentroidCompare & a, const LBCommon::LBCentroidCompare & b){
-    return a.distance < b.distance;
-  };
+    return a.distance < b.distance;/*{{{*/
+  };/*}}}*/
 
+
+  struct NdComparator{
+    int dim = -1;/*{{{*/
+    NdComparator(int dim_){
+      dim = dim_;
+    };
+
+    bool operator() (LBCommon::LBCentroidAndIndexRecord a, LBCommon::LBCentroidAndIndexRecord b){
+      return a.centroid[dim] < b.centroid[dim];
+    };
+  };
+/*}}}*/
 }
 
 #endif //PARATREET_USER_LBCOMMON_H_
