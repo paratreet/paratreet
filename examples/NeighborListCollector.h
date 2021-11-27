53296368998216130fbb743103c771643f7d400d
Joseph Hutter <tg850493@c455-012.stampede2.tacc.utexas.edu>
2021-11-27 12:24:16 -0600

    cleanup SPH

--- examples/NeighborListCollector.h
+++ examples/NeighborListCollector.h
@@ -36,6 +36,9 @@ struct NeighborListCollector : public CBase_NeighborListCollector {
     for (auto && forward_pe : pes_requested) {
       thisProxy[forward_pe].fillRequest(pe_home, part);
     }
+    auto& rp = remote_particles[part.key];
+    rp.first.partition_home_pe = pe_home;
+    rp.second.key = part.key;
   }
   void fillRequest(int pe_home, Particle part) {
     part.acceleration = Vector3D<Real>(0,0,0);
@@ -55,7 +58,6 @@ struct NeighborListCollector : public CBase_NeighborListCollector {
   // the particles partition pe to its subtree pe, which is its leaf.home_pe
   void densityFinished(const Particle& part, const SpatialNode<CentroidData>& leaf) {
     thisProxy[leaf.home_pe].forwardRequest(thisIndex, part); // needed for RTFORCE
-    thisProxy[leaf.home_pe].savePartitionHome(thisIndex, part);
     // send density forward
   }
   void saveSubtreeHome(int subtree_home_pe, Particle part) {
@@ -63,41 +65,50 @@ struct NeighborListCollector : public CBase_NeighborListCollector {
     rp.first.subtree_home_pe = subtree_home_pe;
     rp.second.key = part.key;
   }
-  void savePartitionHome(int partition_home_pe, Particle part) {
-    auto& rp = remote_particles[part.key];
-    rp.first.partition_home_pe = partition_home_pe;
-    rp.second.key = part.key;
-  }
   void shareAccelerations() {
+    std::map<int, std::vector<Particle>> addAccelerations, forwardAccelerations;
     for (auto && remote_part : remote_particles) {
       auto home_state = remote_part.second.first;
       if (home_state.partition_home_pe > -1) {
         if (home_state.partition_home_pe != thisIndex) { // dont want to double add
-          thisProxy[home_state.partition_home_pe].addAcceleration(remote_part.second.second);
+          addAccelerations[home_state.partition_home_pe].push_back(remote_part.second.second);
         }
       }
       else {
-        thisProxy[home_state.subtree_home_pe].forwardAcceleration(remote_part.second.second);
+        forwardAccelerations[home_state.subtree_home_pe].push_back(remote_part.second.second);
       }
       // give it to someone else
       // then += all the remote contributions
       // then average
     }
+    for (auto && fa : forwardAccelerations) {
+      thisProxy[fa.first].forwardAccelerations(fa.second);
+    }
+    for (auto && fa : addAccelerations) {
+      thisProxy[fa.first].addAccelerations(fa.second);
+    }
   }
-  void addAcceleration(Particle part) {
-    auto it = remote_particles.find(part.key);
-    CkAssert(it != remote_particles.end());
-    it->second.second.acceleration += part.acceleration;
-    it->second.second.pressure_dVolume += part.pressure_dVolume;
+  void addAccelerations(const std::vector<Particle>& parts) {
+    for (auto& part : parts) {
+      auto it = remote_particles.find(part.key);
+      CkAssert(it != remote_particles.end());
+      it->second.second.acceleration += part.acceleration;
+      it->second.second.pressure_dVolume += part.pressure_dVolume;
+    }
   }
-  void forwardAcceleration(Particle part) {
-    auto it = remote_particles.find(part.key);
-    CkAssert(it != remote_particles.end());
-    auto pHome = it->second.first.partition_home_pe;
-    CkAssert(pHome > -1);
-    thisProxy[pHome].addAcceleration(part);
+  void forwardAccelerations(const std::vector<Particle>& parts) {
+    std::map<int, std::vector<Particle>> forwardAccelerations;
+    for (auto& part : parts) {
+      auto it = remote_particles.find(part.key);
+      CkAssert(it != remote_particles.end());
+      auto pHome = it->second.first.partition_home_pe;
+      CkAssert(pHome > -1);
+      forwardAccelerations[pHome].push_back(part);
+    }
+    for (auto && fa : forwardAccelerations) {
+      thisProxy[fa.first].addAccelerations(fa.second);
+    }
   }
-
 };
 
 #endif
