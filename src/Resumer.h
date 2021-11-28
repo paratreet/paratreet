aae65a1518b125fe46a13e4baab272a01ee49540
Joseph Hutter <tg850493@login3.stampede2.tacc.utexas.edu>
2021-11-27 18:33:31 -0600

    allow simulatenous traversal

--- src/Resumer.h
+++ src/Resumer.h
@@ -15,32 +15,34 @@ public: // these need to be seen by other local chares
   CProxy_Partition<Data> part_proxy;
   CProxy_Subtree<Data> subtree_proxy;
   CacheManager<Data>* cm_local;
-  std::vector<std::queue<Node<Data>*>> resume_nodes_per_part;
-  std::unordered_map<Key, std::vector<int>> waiting;
+  std::vector<std::vector<std::queue<Node<Data>*>>> all_resume_nodes;
+  std::unordered_map<Key, std::vector<std::pair<int, int>>> waiting;
   bool use_subtree = false;
 
   void reset() {
 #if DEBUG
-    for (auto && rnq : resume_nodes_per_part) {
-      if (!rnq.empty()) CkAbort("did not complete last traversal");
+    for (auto& trav : all_resume_nodes) {
+      for (auto && rnq : trav) {
+        if (!rnq.empty()) CkAbort("did not complete last traversal");
+      }
     }
     CkAssert(waiting.empty()); // should have gotten rid of them
 #endif
   }
 
   void process(Key key) {
-    CkAssert(!resume_nodes_per_part.empty());
+    CkAssert(!all_resume_nodes.empty());
     auto node = cm_local->root->getDescendant(key);
     CkAssert(node && node->key == key);
     auto it = waiting.find(key);
     if (it == waiting.end()) return;
-    for (auto part_index : it->second) {
-      auto && resume_nodes = resume_nodes_per_part[part_index];
+    for (auto pair : it->second) { // (trav_idx, part_idx)
+      auto && resume_nodes = all_resume_nodes[pair.first][pair.second];
       bool should_resume = resume_nodes.empty();
       resume_nodes.push(node);
       if (should_resume) {
-        if (use_subtree) subtree_proxy[part_index].goDown();
-        else part_proxy[part_index].goDown();
+        if (use_subtree) subtree_proxy[pair.second].goDown(pair.first);
+        else part_proxy[pair.second].goDown(pair.first);
       }
     }
     waiting.erase(it);
